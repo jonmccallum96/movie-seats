126d9f63a8b46356de0a7d5a0a0a548fc19d79a5
Jon <jon.a.mccallum@gmail.com>
2020-12-11 16:32:12 +0000

    add local storage data saving

--- script.js
+++ script.js
@@ -4,24 +4,45 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const filmSelect = document.getElementById('movie');
 
-const seatPrice = +filmSelect.value;
+let seatPrice = +filmSelect.value;
+
+//SAVE SELECTED MOVIE INDEX + SEAT PRICE
+
+function setFilmData(filmIndex, filmPrice) {
+  localStorage.setItem('selectedFilmIndex', filmIndex);
+  localStorage.setItem('selectedFilmPrice', filmPrice);
+}
 
 
 //UPDATE SEAT SELECTION COUNT AND TOTAL PRICE
 function updateSelectedCount() {
   const seatsSelected = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...seatsSelected].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const countseatsSelected = seatsSelected.length;
+  const totalPrice = countseatsSelected * seatPrice;
   count.innerText = countseatsSelected;
+  total.innerText = totalPrice;
 
 }
 
+//HANDLE FILM SELECTION
+filmSelect.addEventListener('change', (e) => {
+  seatPrice = +e.target.value;
+  setFilmData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+})
+
+//HANDLE SEAT CLICKS
 container.addEventListener('click', (e) => {
 
   const classList = e.target.classList;
 
   if (classList.contains('seat') && !classList.contains('occupied')) {
     classList.toggle('selected');
-
     updateSelectedCount();
   }
 
