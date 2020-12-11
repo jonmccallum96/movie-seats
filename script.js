2cb20804349405f12d68bf22c100a92f906e0ea7
Jon <jon.a.mccallum@gmail.com>
2020-12-11 15:59:07 +0000

    add js seat selection basic

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const filmSelect = document.get
+const filmSelect = document.getElementById('movie');
+
+const seatPrice = +filmSelect.value;
+
+
+//UPDATE SEAT SELECTION COUNT AND TOTAL PRICE
+function updateSelectedCount() {
+  const seatsSelected = document.querySelectorAll('.row .seat .selected');
+}
+
+container.addEventListener('click', (e) => {
+
+  const classList = e.target.classList;
+
+  if (classList.contains('seat') && !classList.contains('occupied')) {
+    classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+
+});
